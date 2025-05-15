9771b4c7a6462c75028fd05c5a512e7ffe264698
/sable <hl4khk3t@duck.com>
2025-05-15 14:54:48 -0700

    future proofing version with the ^

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if command -v pandoc >/dev/null 2>&1; then
     pandoc -t revealjs \
     --variable theme="black" \
     -s -o index.html slides.md \
-    -V revealjs-url=https://unpkg.com/reveal.js@5.2.1
+    -V revealjs-url=https://unpkg.com/reveal.js@^5
 
     echo "Opening index.html"
     open index.html
